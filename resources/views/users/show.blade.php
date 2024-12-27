395f18d570f6dda1f4eeb8d53ec5aac35409d83a
luongthitam <luong_thi_tam@gdo.co.jp>
2024-12-27 14:38:45 +0900

    post

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
     <div class="sm:grid sm:grid-cols-3 sm:gap-10">
-        <aside class="mt-4">
+        <aside class="mt-4 mb-2">
             {{-- ユーザー情報 --}}
             @include('users.card')
         </aside>
+        <div class="sm:col-span-2 mt-4">
+            {{-- 投稿フォーム --}}
+            @include('microposts.form')
+            {{-- 投稿一覧 --}}
+            @include('microposts.microposts')
+        </div>
     </div>
 @endsection
